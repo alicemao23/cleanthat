3d77c00b598e34d8f3f55a9c9cbcc88c58263943
Alice M <amao@tribalscale.com>
2020-11-01 21:21:57 -0500

    fix(MobileMenu): fix ui on menu item placement
     * add swipeable close

--- src/components/Menu/MobileMenu.js
+++ src/components/Menu/MobileMenu.js
@@ -1,20 +1,22 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { navigate } from 'gatsby'
-
-import Drawer from '@material-ui/core/Drawer'
+import { makeStyles } from '@material-ui/core/styles'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 
 import Button from '../Button'
-
 import { NavLink } from '../Links'
+
+import CloseIcon from '../../media/close-icon.svg'
 
 const MenuContainer = styled.div`
   display: flex;
   flex-direction: column;
   min-width: 375px;
   justify-content: space-between;
+  align-items: center;
   height: 100vh;
   padding: 3rem 3.5rem;
 `
@@ -23,29 +25,68 @@ const Nav = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 100%;
-  > * :not(:last-child) {
-    // margin: 0 45px;
+  position: relative;
+  height: max-content;
+  width: 100%;
+  > * :not(:last-child):not(:first-child) {
+    margin: 2.5rem auto;
   }
 `
-const MenuDrawer = () => {
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    fill: theme.colors.accent
+  },
+  closeBtn: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: 25,
+    height: 25
+  }
+}))
+const MenuDrawer = ({ closeMenu }) => {
+  const classes = useStyles()
   return (
     <MenuContainer>
       <Nav>
-        <NavLink key={1} to="/" activeClassName="active">
+        <IconButton onClick={closeMenu} className={classes.closeBtn}>
+          <img src={CloseIcon} className={classes.icon} alt="close menu" />
+        </IconButton>
+        <NavLink onClick={closeMenu} key={1} to="/" activeClassName="active">
           Home
         </NavLink>
-        <NavLink key={2} to="about-us" activeClassName="active">
+        <NavLink
+          onClick={closeMenu}
+          key={2}
+          to="about-us"
+          activeClassName="active"
+        >
           About us
         </NavLink>
-        <NavLink key={3} to="commercial-services" activeClassName="active">
+        <NavLink
+          onClick={closeMenu}
+          key={3}
+          to="commercial-services"
+          activeClassName="active"
+        >
           Commercial Services
         </NavLink>
-        <NavLink key={4} to="residential-services" activeClassName="active">
+        <NavLink
+          onClick={closeMenu}
+          key={4}
+          to="residential-services"
+          activeClassName="active"
+        >
           Residential Services
         </NavLink>
 
-        <NavLink key={5} to="contact-us" activeClassName="active">
+        <NavLink
+          onClick={closeMenu}
+          key={5}
+          to="contact-us"
+          activeClassName="active"
+        >
           Contact us
         </NavLink>
         {/* <NavLink key={6} to="blog" activeClassName="active">
@@ -81,9 +122,9 @@ const MobileMenu = () => {
       <IconButton onClick={toggleDrawer}>
         <MenuIcon />
       </IconButton>
-      <Drawer open={open} onClose={closeDrawer}>
-        <MenuDrawer />
-      </Drawer>
+      <SwipeableDrawer anchor="right" open={open} onClose={closeDrawer}>
+        <MenuDrawer closeMenu={closeDrawer} />
+      </SwipeableDrawer>
     </>
   )
 }
