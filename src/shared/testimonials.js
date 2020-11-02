5415b56f859632ab70afc8d8ea96e9b9d54c6394
Alice M <amao@tribalscale.com>
2020-11-02 18:44:47 -0500

    feat(Testimonial): add stock images for commercial & residential testimonials

--- src/shared/testimonials.js
+++ src/shared/testimonials.js
@@ -4,21 +4,21 @@ const TESTIMONIALS = [
     details:
       'The staff is AMAZING. They have gone above and beyond for our company numerous times and are always there to lend a little bit of an extra helping hand when we need it. Thank you!',
     imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60'
+      'https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80'
   },
   {
     author: 'Christine, PCK',
     details:
       'We are extremely happy with the level of service CleanThat provides. You can trust their staff to be very thorough and clean. The microwave and keyboards were spotless, even our sugar bowl for coffee was sparkling. It’s the little extra care they take that make a big difference. Rest assured they will leave your office smelling fresh and clean. We highly recommend CleanThat!',
     imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60'
+      'https://images.unsplash.com/photo-1497366754035-f200968a6e72?ixlib=rb-1.2.1&auto=format&fit=crop&w=2301&q=80'
   },
   {
     author: 'Kaitlyn Brouse, Anomoly',
     details:
       "We've been using cleanthat for the past 9 months and have been very pleased with the service! Our office is located downtown Toronto and the team comes in and always does a thorough job for us. Working with Adrian and Johnathan has been very easy and communication is a breeze. Thanks to the team at CleanThat!",
     imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60'
+      'https://images.unsplash.com/photo-1497215457980-d57c69aee12d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80'
   }
 ]
 
@@ -28,21 +28,21 @@ export const RESIDENTIAL_TESTIMONIALS = [
     details:
       'Had a great experience with CleanThat! I’ve used other residential cleaning services before but CleanThat was definitely the best in terms of value and quality. Easy to book and the staff that came to clean my apartment did a great job for both deep and light cleaning tasks.',
     imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60'
+      'https://images.unsplash.com/photo-1556912172-45b7abe8b7e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80'
   },
   {
     author: 'Michael Sheedy',
     details:
       "A friend recommended these guys to us and we couldn't be happier. They did an amazing job cleaning up after a renovation. Highly recommend them!",
     imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60'
+      'https://images.unsplash.com/photo-1534349762230-e0cadf78f5da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80'
   },
   {
     author: 'Andre Proulx',
     details:
       'Incredibly reliable. All stress and frustration around keeping things consistently clean disappeared once we hired them.',
     imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60'
+      'https://images.unsplash.com/photo-1513694203232-719a280e022f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3298&q=80'
   }
 ]
 export default TESTIMONIALS
